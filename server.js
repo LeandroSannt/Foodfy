572cb72badcc854d71f87b3bfee7a5c05348689f
leandro santos do nascimento <lsn_slim@yahoo.com.br>
2020-07-14 22:43:52 -0300

    Refatorando foodfy

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const nunjucks = require("nunjucks")
+
+const server = express()
+
+server.use(express.static("public"))
+
+server.set("view engine" , "njk")
+
+nunjucks.configure("Views",{
+    express:server,
+    autoescape:false,
+    noCache:true
+})
+
+/*=====ROUTES=====*/
+server.get("/", function(req,res){
+    return res.render("index")
+})
+
+server.get("/sobre",function(req,res){
+    return res.render("sobre")
+})
+
+server.get("/receitas",function(req,res){
+    return res.render("receitas")
+})
+
+
+
+
+/*==================*/
+
+
+server.listen(5000,function(){
+    console.log("Server is running")
+})
