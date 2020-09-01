10115641a8303603383939dce521672d9197c84d
leandro santos do nascimento <lsn_slim@yahoo.com.br>
2020-09-01 17:24:36 -0300

    detalhes

--- public/scripts.js
+++ public/scripts.js
@@ -14,4 +14,21 @@ for (let i = 0; i < click.length; i++) {
     })
 }
 
+document.querySelector(".newIngre").addEventListener("click",function(){
+    const newFieldContainer = document.querySelector(".ingredient").cloneNode(true)
+    const fileds = newFieldContainer.querySelectorAll("input")
+    fileds.forEach(function(field){
+        field.value =""
+
+    const newIngredient=document.querySelector(".ingredient img")
+    newIngredient.classList.add("visible")
+    })
+   const childField= document.querySelector("#ingredients").appendChild(newFieldContainer)
+  
+})
+
+
+
+
+
 
