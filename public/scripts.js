6985664979ca633976b789e4e13f0cb96d0c6593
leandro santos do nascimento <lsn_slim@yahoo.com.br>
2020-09-02 10:46:26 -0300

    front admin ok

--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,6 @@
 const click = document.querySelectorAll("span")
 const lista = document.querySelectorAll(".lista")
-
+console.log(click)
 
 for (let i = 0; i < click.length; i++) {
     click[i].addEventListener("click", function () {
@@ -13,19 +13,49 @@ for (let i = 0; i < click.length; i++) {
         }
     })
 }
+    const newIngre = document.querySelector(".newIngre")
+    console.log(newIngre)
 
-document.querySelector(".newIngre").addEventListener("click",function(){
-    const newFieldContainer = document.querySelector(".ingredient").cloneNode(true)
-    const fileds = newFieldContainer.querySelectorAll("input")
-    fileds.forEach(function(field){
+   // for(let a = 0; a<newIngre.length; a++){
+        newIngre.addEventListener("click",function(){
+        const newFieldContainer = document.querySelector(".ingredient").cloneNode(true)
+        const fileds = newFieldContainer.querySelectorAll("input")
+        fileds.forEach(function(field){
+            field.value =""
+
+    })
+document.querySelector("#ingredients").appendChild(newFieldContainer)
+})
+
+
+const newPrep = document.querySelector(".newPrep")
+console.log(newIngre)
+
+// for(let a = 0; a<newIngre.length; a++){
+    newPrep.addEventListener("click",function(){
+    const newFieldContainer = document.querySelector(".preparation").cloneNode(true)
+    const fields = newFieldContainer.querySelectorAll("input")
+    fields.forEach(function(field){
         field.value =""
 
-    const newIngredient=document.querySelector(".ingredient img")
-    newIngredient.classList.add("visible")
-    })
-   const childField= document.querySelector("#ingredients").appendChild(newFieldContainer)
-  
 })
+document.querySelector("#preparation").appendChild(newFieldContainer)
+})
+/*
+    document.querySelector(".ingredient img").addEventListener("click",function(){
+    const removeIngre =document.querySelector("#addIngre")
+    removeIngre.removeChild(removeIngre.childNodes[1])
+   
+})*/
+
+
+
+
+
+
+
+
+
 
 
 
