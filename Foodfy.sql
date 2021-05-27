a1538d5f7298f9b489285f42e62343eb873e306e
LeandroSannt <lsn_slim@yahoo.com.br>
2021-05-27 13:41:02 -0300

    adicionando sql

---
+++ Foodfy.sql
@@ -0,0 +1,128 @@
+DROP DATABASE IF EXISTS foodfy;
+CREATE DATABASE foodfy;
+
+CREATE TABLE "recipes" (
+	"id" SERIAL PRIMARY KEY,
+  "title" text NOT NULL,
+  "chef_id" int NOT NULL,
+  "user_id" int,
+  "ingredients" text[] NOT NULL,
+  "preparation" text[] NOT NULL,
+  "information" text NOT NULL,
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now())
+);
+  
+CREATE TABLE "recipe_files" (
+	"id" SERIAL PRIMARY KEY,
+  "recipe_id" int NOT NULL,
+  "file_id" int NOT NULL 
+);
+
+CREATE TABLE "files" (
+	"id" SERIAL PRIMARY KEY,
+  "name" text NOT NULL,
+  "path" text NOT NULL
+);
+
+CREATE TABLE "chefs" (
+	"id" SERIAL PRIMARY KEY,
+  "name" text NOT NULL,
+  "file_id" int UNIQUE NOT NULL,
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now())
+);
+
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY,
+  "name" TEXT NOT NULL,
+  "email" TEXT UNIQUE NOT NULL,
+  "password" TEXT NOT NULL,
+  "reset_token" TEXT,
+  "reset_token_expires" TEXT,
+  "is_admin" BOOLEAN DEFAULT false,
+  "created_at" TIMESTAMP DEFAULT(now()),
+  "updated_at" TIMESTAMP DEFAULT(now())
+);
+
+CREATE TABLE 'session' (
+  "sid" varchar NOT NULL COLLATE "default",
+  "sess" json NOT NULL,
+  "expire" timestamp(6) NOT NULL
+)
+WITH (OIDS=FALSE);
+ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY key ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+
+ALTER TABLE "recipes" ADD FOREIGN KEY ("chef_id") REFERENCES "chefs" ("id");
+ALTER TABLE "chefs" ADD FOREIGN KEY ("file_id") REFERENCES "files" ("id");
+
+
+
+ALTER TABLE "recipe_files" ADD FOREIGN KEY ("recipe_id") REFERENCES "recipes" ("id") ON DELETE CASCADE;
+-- aqui ta o problema
+
+ALTER TABLE "recipe_files" ADD FOREIGN KEY ("file_id") REFERENCES "files" ("id") ON DELETE CASCADE;
+
+ALTER TABLE "files" ADD FOREIGN KEY ("id") REFERENCES "recipe_files" ("file_id") ON DELETE CASCADE;
+
+ALTER TABLE "recipes" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE;
+
+
+CREATE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW ();
+  RETURN NEW;	
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON recipes
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON chefs
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+-- INSERT INTO recipes(name, file_id) VALUES('Cleide, ');
+
+
+
+
+SELECT files.*, chefs.file_id AS file_id
+        FROM files 
+        LEFT JOIN recipe_files ON (recipe_files.file_id = files.id)
+        WHERE chefs.recipe_id = 1
+        
+        SELECT  recipes.*, chefs.name AS chef_recipes 
+        FROM recipes 
+        LEFT JOIN chefs ON(recipes.chef_id = chefs.id)
+        WHERE  recipes.id = 2
+        
+        
+  CREATE TABLE "session" (
+  "sid" varchar NOT NULL COLLATE "default",
+  "sess" json NOT NULL,
+  "expire" timestamp(6) NOT NULL
+)
+WITH (OIDS=FALSE);
+ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY key ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+
+ALTER TABLE "users" ADD COLUMN reset_token text;
+ALTER TABLE "users" ADD COLUMN reset_token_expires text;
+
+ALTER TABLE "products"
+DROP CONSTRAINT products_user_id_fkey,
+ADD CONSTRAINT products_user_id_fkey
+FOREIGN KEY ("user_id")
+REFERENCES "users" ("id")
+ON DELETE CASCADE;
+
+ALTER TABLE "files"
+DROP CONSTRAINT files_product_id_fkey,
+ADD CONSTRAINT files_product_id_fkey
+FOREIGN KEY ("product_id")
+REFERENCES "products" ("id")
+ON DELETE CASCADE;
